789299ddbd28781794c00f12d58b2f56df9a5573
Andrewgaabriel <andrew.gabrielgomes@gmail.com>
2021-09-22 14:24:32 -0300

    Corrige identações Davy.java;

--- fonte/Davy.java
+++ fonte/Davy.java
@@ -11,27 +11,3 @@ public static void main(String[] args) {
         scanner.LeTudo(arquivo);    
     }
 }
-
-
-
-
-
-
-
-
-
-
-/* try {
-        File file = new File("teste.davy");
-        Scanner input = new Scanner(file);
-        while (input.hasNextLine()) {
-            String line = input.nextLine();
-            System.out.println(line);
-        }
-    input.close();
-    } catch (Exception e) {
-        System.out.println("Nao foi possivel abrir o arquivo teste.davy.");
-        System.out.println("Ele existe mesmo?");
-
-        e.printStackTrace();
-    } */
