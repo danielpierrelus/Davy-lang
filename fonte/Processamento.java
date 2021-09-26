386c5ea45ccdf3c9bac85a3181f6b0461bc2d73c
kerby <lovincekerby99@gmail.com>
2021-09-25 21:46:20 -0300

    commit

--- fonte/Processamento.java
+++ fonte/Processamento.java
@@ -1,16 +1,33 @@
 package fonte;
 
 public class Processamento {
-    public String Linhas[];
+    public Variavel V[];
+
+    //public String Linhas[];
     
 
-    public Processamento(String VLinhas[]){
-        this.Linhas = VLinhas;
+    public Processamento(){
+        V = new Variavel[200];
+            
+        };
+
+    public void CriarVariavel(String g){
+        for (int i = 0; i<V.length; i++){
+            if (this.V[i] == null) {
+                if(g.startsWith("int")){
+                    if (ChangeVariavel(getName(g),getValue(g)));
+    //Pesquisa se a variavél ja existe, caso já tenha uma com o mesmo nome ele subscrever ela
+                }
+
+            }
+        }
+    }
+        //this.Linhas = VLinhas;
         //declarar as variáveis; usar HashMap? Map?
     }
     
 
-    public void processa(){
+    //public void processa(){
 
     }
 }
